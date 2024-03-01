c7671743b13a72d30c3cc1bf9ad37ea6489788a5
Ana <anagoncalves7@icloud.com>
2024-03-01 09:53:24 +0100

    new file

---
+++ receive.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = "vlbelintrocrypto.hevs.ch"
+port = 6000
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#functions
+def receive_message():
+    rcv_msg = sock.recv(65536) # 64Ko
+    rcv_msg = rcv_msg.decode('utf-8')
+    rcv_msg = rcv_msg[4:]
+    print(rcv_msg)
+
+sock.connect((HOST,port))
+#sock.send(msg_final)
+while True :
+    receive_message
+   
+        
+ 
+
